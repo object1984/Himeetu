91d968d1517ce3f0b0907d3322bc030c8d14a1b6
object1984 <object1984@gmail.com>
2015-12-19 10:07:48 +0800

    update  login & register

--- app/src/main/java/com/himeetu/ui/register/CountryChooseActivity.java
+++ app/src/main/java/com/himeetu/ui/register/CountryChooseActivity.java
@@ -14,6 +14,7 @@ import com.himeetu.R;
 import com.himeetu.adapter.SortAdapter;
 import com.himeetu.app.NavHelper;
 import com.himeetu.model.Country;
+import com.himeetu.network.dic.Argument;
 import com.himeetu.ui.base.BaseActivity;
 import com.himeetu.util.CountryComparator;
 import com.himeetu.view.SideBar;
@@ -31,14 +32,14 @@ public class CountryChooseActivity extends BaseActivity implements View.OnClickL
     private TextView dialog;
     private SortAdapter adapter;
     private List<Country> SourceDateList;
-
     private Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setThemeTranslucent();
         setContentView(R.layout.activity_country_choose);
-
+        setupToolbar(false, R.string.your_country);
         init();
     }
 
@@ -113,7 +114,7 @@ public class CountryChooseActivity extends BaseActivity implements View.OnClickL
     @Override
     protected void initViews() {
         super.initViews();
-//        setTitleText(R.string.your_country);
+
     }
 
     @Override
@@ -126,6 +127,9 @@ public class CountryChooseActivity extends BaseActivity implements View.OnClickL
     }
 
     private void goNext(){
-        NavHelper.toIdentityPage(this);
+        int countryCode = sortListView.getSelectedItemPosition();
+        String username = getIntent().getStringExtra(Argument.USERNAME);
+        String password = getIntent().getStringExtra(Argument.PASSWORD);
+        NavHelper.toIdentityPage(this, username, password, countryCode);
     }
 }
